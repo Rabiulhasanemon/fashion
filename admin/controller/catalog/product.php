c548ff2af11e64f0d56a738c70893ceb0119fd9d
Rabiul Hasan <rabiulhasanemon@gmail.com>
2025-12-13 00:27:48 +0600

    Set default product list sort to show newest products first (product_id DESC)

--- admin/controller/catalog/product.php
+++ admin/controller/catalog/product.php
@@ -692,13 +692,13 @@ class ControllerCatalogProduct extends Controller {
 		if (isset($this->request->get['sort'])) {
 			$sort = $this->request->get['sort'];
 		} else {
-			$sort = 'pd.name';
+			$sort = 'p.product_id';
 		}
 
 		if (isset($this->request->get['order'])) {
 			$order = $this->request->get['order'];
 		} else {
-			$order = 'ASC';
+			$order = 'DESC';
 		}
 
 		if (isset($this->request->get['page'])) {
