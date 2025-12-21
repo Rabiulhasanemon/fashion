a5e7ddba66f63ea3c230b067302c8e980e2756de
Rabiulhasanemon <rabiulhasanemon@gmail.com>
2025-12-21 15:52:04 +0600

    Update SQL file with correct table prefix (sr_)

--- create_fbt_table.sql
+++ create_fbt_table.sql
@@ -1,5 +1,8 @@
 -- Create Frequently Bought Together table
-CREATE TABLE IF NOT EXISTS `oc_product_frequently_bought_together` (
+-- IMPORTANT: Replace 'sr_' with your actual DB_PREFIX if different
+-- Your current prefix is: sr_ (as seen in config.php)
+
+CREATE TABLE IF NOT EXISTS `sr_product_frequently_bought_together` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `product_id` int(11) NOT NULL,
   `fbt_product_id` int(11) NOT NULL,
@@ -10,4 +13,8 @@ CREATE TABLE IF NOT EXISTS `oc_product_frequently_bought_together` (
   KEY `fbt_product_id` (`fbt_product_id`),
   UNIQUE KEY `unique_product_fbt` (`product_id`, `fbt_product_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- Alternative: Use this if you want to use a variable prefix
+-- Replace {PREFIX} with your actual table prefix (sr_)
+-- CREATE TABLE IF NOT EXISTS `{PREFIX}product_frequently_bought_together` (
 
